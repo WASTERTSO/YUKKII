7a10de9932fd70edc92fe2b826f27e6c8f838a66
|•| Tʜᴇ Sʜɪᴠ Oғғɪᴄɪᴀɪʟ ⋟ <126413724+WASTERTSO@users.noreply.github.com>
2023-12-11 17:03:45 +0530

    Update start.py

--- YukkiMusic/utils/inline/start.py
+++ YukkiMusic/utils/inline/start.py
@@ -62,7 +62,7 @@ def private_panel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
     buttons = [
         [
             InlineKeyboardButton(
-                text=_["S_B_5"], callback_data="settings_back_helper"
+                text=_["S_B_5"], url=f"https://t.me/{BOT_USERNAME}?startgroup=true",
             )
         ]
     ]
@@ -97,17 +97,16 @@ def private_panel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
     buttons.append(
         [
             InlineKeyboardButton(
-                text=_["S_B_8"],
-                url=f"https://t.me/{BOT_USERNAME}?startgroup=true",
+                text=_["S_B_8"], callback_data="settings_back_helper"
+                
             )
         ]
     )
-    if GITHUB_REPO and OWNER:
+    if SUPPORT_GROUP and OWNER:
         buttons.append(
             [
                 InlineKeyboardButton(text=_["S_B_7"], user_id=OWNER),
-                InlineKeyboardButton(
-                    text=_["S_B_6"], url=f"{GITHUB_REPO}"
+                InlineKeyboardButton(text=_["S_B_6"], url=f"SUPPORT_GROUP"
                 ),
             ]
         )
