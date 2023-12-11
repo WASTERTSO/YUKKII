d2baf5e257d1592a39abac3081f9028a02cc721f
|•| Tʜᴇ Sʜɪᴠ Oғғɪᴄɪᴀɪʟ ⋟ <126413724+WASTERTSO@users.noreply.github.com>
2023-12-11 19:20:56 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -66,10 +66,8 @@ GIT_TOKEN = getenv("GIT_TOKEN", None)
 # Only  Links formats are  accepted for this Var value.
 SUPPORT_CHANNEL = getenv(
     "SUPPORT_CHANNEL", "https://t.me/godx_bots")
-)  
 SUPPORT_GROUP = getenv(
     "SUPPORT_GROUP", "https://t.me/tso_chats")
-
 
 )  
 # Set it in True if you want to leave your assistant after a certain amount of time. [Set time via AUTO_LEAVE_ASSISTANT_TIME]
